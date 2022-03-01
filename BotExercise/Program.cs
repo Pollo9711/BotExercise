13079245c64394e1b52ea0e3cc1bd937df1db886
Andrea Pollini <andrea.pollini@eositaly.it>
2022-03-01 23:24:54 +0100

    import project

---
+++ BotExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace BotExercise
+{
+    internal class Program
+    {
+        public static TelegramBotClient BotClient;
+
+        static void Main(string[] args)
+        {
+            BotClient = new TelegramBotClient("5266813309:AAHOOC0zxmCyU-eJJA5Zf_31yYFIHnmgz6A");
+
+            var me = BotClient.GetMeAsync().Result;
+
+            Console.WriteLine($"Hello {me.Username}!");
+
+            
+        }
+    }
+}
